50d9740004c88e8b7214bc8e75a39f8dd596e5d6
marcio <78marcio@gmail.com>
2026-06-18 13:43:58 +0200

    .

--- src/components/ThemeSettingsControl.jsx
+++ src/components/ThemeSettingsControl.jsx
@@ -114,7 +114,7 @@ export default function ThemeSettingsControl({
                   style={{
                     background: themeColor === c.name ? S.surface : S.surfaceLo,
                     border: `1px solid ${themeColor === c.name ? S.borderHi : S.border}`,
-                    boxShadow: cardShadow,
+                   
                   }}>
                   <div className="w-3 h-3 rounded-full shrink-0"
                     style={{ background: c.value, boxShadow: swatchRing }} />
