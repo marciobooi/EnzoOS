69f57048b6f4dea9e6739d0633585f49741e71a7
marcio <78marcio@gmail.com>
2026-07-02 15:20:08 +0200

    .

--- src/components/ResonanceLogo.jsx
+++ src/components/ResonanceLogo.jsx
@@ -152,7 +152,6 @@ export default function ResonanceLogo({ className = '', style, bare = false }) {
             <span className="rl-txt">res</span>
             <span className="rl-play-o" />
             <span className="rl-txt">nance</span>
-            <span className="rl-txt">2.0</span>
           </div>
         </div>
       </div>
